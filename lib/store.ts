169ad4cabb9b3fb54339c9a717df0ec6a4bb5f14
carlosleyvarosaldo <carlos.leyvarosaldo@gmail.com>
2026-04-29 01:00:19 -0500

    Update store.ts

--- lib/store.ts
+++ lib/store.ts
@@ -1,29 +1,40 @@
 import { create } from "zustand";
-import { Estimate } from "@/types";
+import { Estimate, Tree } from "@/types";
 import {
   createEstimate as dbCreateEstimate,
   getAllEstimates,
+  addTree as dbAddTree,
+  getTreesForEstimate,
 } from "./db";
 
 type StoreState = {
   activeEstimateId: string | null;
   estimates: Estimate[];
+  activeTrees: Tree[];
+
   setActiveEstimate: (id: string | null) => void;
   createEstimate: () => Promise<Estimate>;
   loadEstimates: () => Promise<void>;
+
+  addTreeToEstimate: (tree: Tree) => Promise<void>;
+  loadTreesForActiveEstimate: () => Promise<void>;
 };
 
-export const useAppStore = create<StoreState>((set) => ({
+export const useAppStore = create<StoreState>((set, get) => ({
   activeEstimateId: null,
   estimates: [],
+  activeTrees: [],
 
-  setActiveEstimate: (id) => set({ activeEstimateId: id }),
+  setActiveEstimate: (id) => {
+    set({ activeEstimateId: id, activeTrees: [] });
+  },
 
   createEstimate: async () => {
     const estimate = await dbCreateEstimate();
     set((state) => ({
       estimates: [estimate, ...state.estimates],
       activeEstimateId: estimate.id,
+      activeTrees: [],
     }));
     return estimate;
   },
@@ -31,5 +42,20 @@ export const useAppStore = create<StoreState>((set) => ({
   loadEstimates: async () => {
     const estimates = await getAllEstimates();
     set({ estimates });
+  },
+
+  addTreeToEstimate: async (tree) => {
+    await dbAddTree(tree);
+    set((state) => ({ activeTrees: [...state.activeTrees, tree] }));
+  },
+
+  loadTreesForActiveEstimate: async () => {
+    const id = get().activeEstimateId;
+    if (!id) {
+      set({ activeTrees: [] });
+      return;
+    }
+    const trees = await getTreesForEstimate(id);
+    set({ activeTrees: trees });
   },
 }));
